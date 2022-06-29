9111cc09fa70c13a2be8e055e100d788edb95a96
gianfrancoscapin <gianscapin96@gmail.com>
2022-06-29 01:38:54 -0300

    fill profile fragment

--- app/src/main/java/com/example/blogapp/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/blogapp/ui/profile/ProfileFragment.kt
@@ -5,10 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
 import com.example.blogapp.R
+import com.example.blogapp.databinding.FragmentProfileBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class ProfileFragment : Fragment(R.layout.fragment_profile) {
+    private lateinit var binding: FragmentProfileBinding
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val user = FirebaseAuth.getInstance().currentUser
+
+        binding = FragmentProfileBinding.bind(view)
+
+        Glide.with(this).load(user?.photoUrl).centerCrop().into(binding.imgProfile)
+
+        binding.nameUser.text = user?.displayName
+
+
     }
 }
